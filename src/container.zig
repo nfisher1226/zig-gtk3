40c5171b5944d5eef9f55791e5439574efa88fa3
Nathan Fisher <jeang3nie@hitchhiker-linux.org>
2021-06-21 18:30:28 -0400

    Added "range" example Numerous bugfixes

--- src/container.zig
+++ src/container.zig
@@ -8,6 +8,10 @@ pub const Container = struct {
         gtk_container_add(self.ptr, widget.ptr);
     }
 
+    pub fn set_border_width(self: Container, border: c_uint) void {
+        gtk_container_set_border_width(self.ptr, border);
+    }
+
     pub fn as_widget(self: Box) Widget {
         return Widget {
             .ptr = @ptrCast(*GtkWidget, self.ptr),
--- src/orientable.zig
+++ src/orientable.zig
@@ -5,9 +5,22 @@ usingnamespace @import("widget.zig");
 pub const Orientable = struct {
     ptr: *GtkOrientable,
 
-    pub fn as_widget(self: Box) Widget {
+    pub fn as_widget(self: Orientable) Widget {
         return Widget {
             .ptr = @ptrCast(*GtkWidget, self.ptr),
         };
+    }
+
+    pub fn get_orientation(self: Orientable) Orientation {
+        const orientation = gtk_orientable_get_orientation(self.ptr);
+        switch (orientation) {
+            orientation_horizontal => return .horizontal,
+            orientation_vertical => return .vertical,
+            else => unreachable,
+        }
+    }
+
+    pub fn set_orientation(self: Orientable, orientation: Orientation) void {
+        gtk_orientable_set_orientation(self.ptr, orientation.parse());
     }
 };

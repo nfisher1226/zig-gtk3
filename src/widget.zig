5a79a12e5c62ba2c2aa314d03f0fe01f17e098d6
Nathan Fisher <jeang3nie@hitchhiker-linux.org>
2022-06-03 15:44:49 -0400

    Additional methods for Widget base class

--- src/widget.zig
+++ src/widget.zig
@@ -111,28 +111,156 @@ pub const Widget = struct {
 
     const Self = @This();
 
+    /// Kinds of widget-specific help. Used by the ::show-help signal.
+    pub const HelpType = enum(c_uint) {
+        /// Tooltip
+        tooltip = c.GTK_WIDGET_HELP_TOOLTIP,
+        /// What's this.
+        whats_this = c.GTK_WIDGET_HELP_WHATS_THIS,
+    };
+
+    /// Reading directions for text.
+    pub const TextDirection = enum(c_uint) {
+        /// No direction.
+        none = c.GTK_TEXT_DIR_NONE,
+        /// Left to right text direction.
+        ltr = c.GTK_TEXT_DIR_LTR,
+        /// Right to left text direction.
+        rtl = c.GTK_TEXT_DIR_RTL,
+    };
+
+    /// Specifies a preference for height-for-width or width-for-height geometry
+    /// management.
+    pub const SizeRequestMode = enum(c_uint) {
+        /// Prefer height-for-width geometry management
+        height_for_width = c.GTK_SIZE_REQUEST_HEIGHT_FOR_WIDTH,
+        /// Prefer width-for-height geometry management
+        width_for_height = c.GTK_SIZE_REQUEST_WIDTH_FOR_HEIGHT,
+        /// Don’t trade height-for-width or width-for-height
+        constant_size = c.GTK_SIZE_REQUEST_CONSTANT_SIZE,
+    };
+
+    /// Controls how a widget deals with extra space in a single (x or y) dimension.
+    ///
+    /// Alignment only matters if the widget receives a “too large” allocation,
+    /// for example if you packed the widget with the “expand” flag inside a Box,
+    /// then the widget might get extra space. If you have for example a 16x16
+    /// icon inside a 32x32 space, the icon could be scaled and stretched, it
+    /// could be centered, or it could be positioned to one side of the space.
+    ///
+    /// Note that in horizontal context .start and .end are interpreted relative
+    /// to text direction.
+    ///
+    /// Align.baseline support for it is optional for containers and widgets,
+    /// and it is only supported for vertical alignment. When its not supported
+    /// by a child or a container it is treated as Align.fill.
     pub const Align = enum(c_uint) {
+        /// stretch to fill all space if possible, center if no meaningful way
+        /// to stretch
         fill = c.GTK_ALIGN_FILL,
+        /// snap to left or top side, leaving space on right or bottom
         start = c.GTK_ALIGN_START,
+        /// snap to right or bottom side, leaving space on left or top
         end = c.GTK_ALIGN_END,
+        /// center natural width of widget inside the allocation
         center = c.GTK_ALIGN_CENTER,
+        /// align the widget according to the baseline. Since 3.10.
         baseline = c.GTK_ALIGN_BASELINE,
     };
 
+    /// Destroys a widget.
+    ///
+    /// When a widget is destroyed all references it holds on other objects will
+    /// be released:
+    /// * if the widget is inside a container, it will be removed from its parent
+    /// * if the widget is a container, all its children will be destroyed,
+    /// recursively
+    /// * if the widget is a top level, it will be removed from the list of top
+    /// level widgets that GTK+ maintains internally
+    ///
+    /// It's expected that all references held on the widget will also be
+    /// released; you should connect to the “destroy” signal if you hold a
+    /// reference to widget and you wish to remove it when this function is
+    /// called. It is not necessary to do so if you are implementing a Container,
+    /// as you'll be able to use the GtkContainerClass.remove() virtual function
+    /// for that.
+    ///
+    /// It's important to notice that gtk_widget_destroy() will only cause the
+    /// widget to be finalized if no additional references, acquired using
+    /// g_object_ref(), are held on it. In case additional references are in
+    /// place, the widget will be in an "inert" state after calling this function;
+    /// widget will still point to valid memory, allowing you to release the
+    /// references you hold, but you may not query the widget's own state.
+    ///
+    /// You should typically call this function on top level widgets, and rarely
+    /// on child widgets.
+    pub fn destroy(self: Widget) void {
+        c.gtk_widget_destroy(self.ptr);
+    }
+
+    /// Flags a widget to be displayed. Any widget that isn’t shown will not
+    /// appear on the screen. If you want to show all the widgets in a container,
+    /// it’s easier to call gtk_widget_show_all() on the container, instead of
+    /// individually showing the widgets.
+    ///
+    /// Remember that you have to show the containers containing a widget, in
+    /// addition to the widget itself, before it will appear onscreen.
+    ///
+    /// When a toplevel container is shown, it is immediately realized and
+    /// mapped; other shown widgets are realized and mapped when their toplevel
+    /// container is realized and mapped.
     pub fn show(self: Self) void {
         c.gtk_widget_show(self.ptr);
     }
 
+    /// Shows a widget. If the widget is an unmapped toplevel widget (i.e. a
+    /// GtkWindow that has not yet been shown), enter the main loop and wait for
+    /// the window to actually be mapped. Be careful; because the main loop is
+    /// running, anything can happen during this function.
     pub fn show_now(self: Self) void {
         c.gtk_widget_show_now(self.ptr);
     }
 
+    /// Reverses the effects of gtk_widget_show(), causing the widget to be
+    /// hidden (invisible to the user).
     pub fn hide(self: Self) void {
         c.gtk_widget_hide(self.ptr);
     }
 
+    /// Recursively shows a widget, and any child widgets (if the widget is a
+    /// container).
     pub fn show_all(self: Self) void {
         c.gtk_widget_show_all(self.ptr);
+    }
+
+    /// Obtains the frame clock for a widget. The frame clock is a global “ticker”
+    /// that can be used to drive animations and repaints. The most common reason
+    /// to get the frame clock is to call gdk_frame_clock_get_frame_time(), in
+    /// order to get a time to use for animating. For example you might record
+    /// the start of the animation with an initial value from
+    /// gdk_frame_clock_get_frame_time(), and then update the animation by calling
+    /// gdk_frame_clock_get_frame_time() again during each repaint.
+    ///
+    /// gdk_frame_clock_request_phase() will result in a new frame on the clock,
+    /// but won’t necessarily repaint any widgets. To repaint a widget, you have
+    /// to use gtk_widget_queue_draw() which invalidates the widget (thus
+    /// scheduling it to receive a draw on the next frame). gtk_widget_queue_draw()
+    /// will also end up requesting a frame on the appropriate frame clock.
+    ///
+    /// A widget’s frame clock will not change while the widget is mapped. 
+    /// Reparenting a widget (which implies a temporary unmap) can change the
+    /// widget’s frame clock.
+    ///
+    /// Unrealized widgets do not have a frame clock.
+    pub fn get_frame_clock(self: Self) *c.GdkFrameClock {
+        return c.gtk_widget_get_frame_clock(self.ptr);
+    }
+
+    /// Retrieves the internal scale factor that maps from window coordinates to
+    /// the actual device pixels. On traditional systems this is 1, on high
+    /// density outputs, it can be a higher value (typically 2).
+    pub fn get_scale_factor(self: Self) c_int {
+        return c.gtk_widget_get_scale_factor(self.ptr);
     }
 
     pub fn is_focus(self: Self) bool {
@@ -243,10 +371,6 @@ pub const Widget = struct {
 
     pub fn set_vexpand(self: Self, expand: bool) void {
         c.gtk_widget_set_vexpand(self.ptr, if (expand) 1 else 0);
-    }
-
-    pub fn destroy(self: Self) void {
-        c.gtk_widget_destroy(self.ptr);
     }
 
     pub fn connect(self: Self, sig: [:0]const u8, callback: c.GCallback, data: ?c.gpointer) void {
